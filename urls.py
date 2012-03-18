cf5740f480e5e22a506b136ce28f338d3e3f7627
Jon Parrish <jonparrish@rocketmail.com>
2012-03-18 00:49:16 -0500

    first commit

---
+++ urls.py
@@ -0,0 +1,31 @@
+from django.conf.urls.defaults import patterns, include, url
+from preview.views import *
+from djecomstore import settings
+
+import os.path
+
+site_media = os.path.join(os.path.dirname(__file__), 'site_media')
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'djecomstore.views.home', name='home'),
+    # url(r'^djecomstore/', include('djecomstore.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+    (r'^catalog/$', home),
+)
+
+
+if settings.DEBUG:
+	urlpatterns += patterns('',
+		(r'^site_media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': site_media}),
+	)
+
